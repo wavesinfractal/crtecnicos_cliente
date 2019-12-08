86c6f5de07cd40b9c907a0be2338aec9b8d9943c
Nulluser <wavesinfractal@example.com>
2019-12-08 13:44:03 -0600

    Prueba del 8/12/2019

--- src/componentes/Dashboard/DashBoard.js
+++ src/componentes/Dashboard/DashBoard.js
@@ -2,44 +2,36 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class DashBoard extends Component {
-  // componentWillUnmount() { 
+  // componentWillUnmount() {
   //   this.props.refetch()
   // }
   render() {
-  
     const { nombre, apellido } = this.props.session
       ? this.props.session
       : { nombre: "", apellido: "" };
     return (
-      
-      
-        
+      <div className="row justify-content-center my-3">
+        <div className="row">
+          <h1 className="text-center h5">Tablero</h1>
+        </div>
+        <div className="col-sm-10 justify-content-center  text-center ">
+          <Link
+            type="button"
+            className="btn btn-lg btn-primary btn-block text-center text-light my-2"
+            to="/ordenes/nuevo"
+          >
+            Reportar Articulo sdfgsdfgsdfgsdf
+          </Link>
 
-     
-        <div className="row justify-content-center my-3">
-        <h1 className="text-center h5">Tablero</h1>
-         <div className="col-sm-10 justify-content-center  text-center ">
-
-           
-       <Link type="button" className="btn btn-lg btn-primary btn-block text-center text-light my-2" to="/ordenes/nuevo">
-             Reportar Articulo Dañado
-            </Link>
-       
-       
-       
-       <Link type="button" className="btn btn-lg btn-primary btn-block  text-center text-light my-2" to="/reportes">
-             Panel de Control
-            </Link>
-       </div>
-        
-         </div>
-        
-
-        
-
-       
-
-  
+          <Link
+            type="button"
+            className="btn btn-lg btn-primary btn-block  text-center text-light my-2"
+            to="/reportes"
+          >
+            Panel de Control
+          </Link>
+        </div>
+      </div>
     );
   }
 }
